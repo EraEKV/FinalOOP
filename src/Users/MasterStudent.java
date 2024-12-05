92c2dca539da48c663d78c8a9941a961b57225e6
Abumuslim Abakarov <abakarovmuslim26@gmail.com>
2024-12-06 01:13:30 +0500

    Add MasterStudent class extending GradStudent with thesis topic and custom research logic

--- src/Users/MasterStudent.java
+++ src/Users/MasterStudent.java
@@ -1,22 +1,28 @@
-package Users ;
+package Users;
 
+public class MasterStudent extends GradStudent {
+	private String thesisTopic;
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
-
-public class MasterStudent extends StudentGradStudent implements CanTeach, CanResearch
-{
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public MasterStudent(){
-		super();
+	public MasterStudent(String name, int age, String researchTopic, String thesisTopic) {
+		super(name, age, researchTopic);
+		this.thesisTopic = thesisTopic;
 	}
 
-}
+	public String getThesisTopic() {
+		return thesisTopic;
+	}
 
+	public void setThesisTopic(String thesisTopic) {
+		this.thesisTopic = thesisTopic;
+	}
+
+	@Override
+	public void research() {
+		System.out.println(getName() + " is conducting research on Master's thesis about " + thesisTopic);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", Thesis Topic: " + thesisTopic;
+	}
+}
