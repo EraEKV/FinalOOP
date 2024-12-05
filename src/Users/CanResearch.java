f85da8f318763e9093e5506f1e0721eb38b0024f
Abumuslim Abakarov <abakarovmuslim26@gmail.com>
2024-12-06 01:14:26 +0500

    cleaned it up

--- src/Users/CanResearch.java
+++ src/Users/CanResearch.java
@@ -1,95 +1,16 @@
-package Users ;
+package Users;
 
+import Academic.Journal;
+import System.ResearchPaper;
+import Enums.Faculty;
+import java.util.Vector;
 
+public interface CanResearch {
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
-public  interface CanResearch 
-{
-    /**
-     * <!-- begin-user-doc -->
-     * <!--  end-user-doc  -->
-     * @generated
-     * @ordered
-     */
-    
-    public  attribute = ;
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public int calculateCitations() ;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public int calculateHIndex() ;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void operation() ;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public Vector<ResearchPaper> printPapers(Request parameter) ;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void publishPaper(ResearchPaper parameter, ResearchJournal parameter2) ;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void topCitedJounalResearcher(Journal parameter) ;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void topCitedResearcher() ;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void topCitedSchoolResearcher(Faculty parameter) ;
-	
-	
+	int calculateHIndex();
+	int calculateCitations();
+	Vector<ResearchPaper> printPapers(Request request);
+	void publishPaper(ResearchPaper paper, Journal journal);
+	void topCitedSchoolResearcher(Faculty faculty);
+	void topCitedResearcher();
 }
-
--- src/Users/CanTeach.java
+++ src/Users/CanTeach.java
@@ -1,23 +1,6 @@
 package Users ;
-
-
-
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
 public  interface CanTeach 
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	public void teach() ;
-	
-	
 }
 
