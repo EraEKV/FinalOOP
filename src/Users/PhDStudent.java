dfb6b6e9ef6926fd0cd022f2e13a6c06e202280b
Abumuslim Abakarov <abakarovmuslim26@gmail.com>
2024-12-06 01:13:40 +0500

    Add PhDStudent class extending GradStudent with dissertation topic and custom research logic

--- src/Users/PhDStudent.java
+++ src/Users/PhDStudent.java
@@ -1,22 +1,28 @@
-package Users ;
+package Users;
 
+public class PhDStudent extends GradStudent {
+	private String dissertationTopic;
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
-
-public class PhDStudent extends StudentGradStudent implements CanTeach, CanResearch
-{
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public PhDStudent(){
-		super();
+	public PhDStudent(String name, int age, String researchTopic, String dissertationTopic) {
+		super(name, age, researchTopic);
+		this.dissertationTopic = dissertationTopic;
 	}
 
-}
+	public String getDissertationTopic() {
+		return dissertationTopic;
+	}
 
+	public void setDissertationTopic(String dissertationTopic) {
+		this.dissertationTopic = dissertationTopic;
+	}
+
+	@Override
+	public void research() {
+		System.out.println(getName() + " is conducting PhD research on " + dissertationTopic);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", Dissertation Topic: " + dissertationTopic;
+	}
+}
