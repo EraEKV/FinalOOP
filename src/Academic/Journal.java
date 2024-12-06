6d2812df88200b5d7c0fb5f10356766dd89e5036
Erasyl Kokenov <kokenaoerasyl@gmail.com>
2024-12-06 18:43:16 +0500

    basic Journal logic

--- src/Academic/Journal.java
+++ src/Academic/Journal.java
@@ -1,127 +1,167 @@
 package Academic ;
 
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
+import Enums.Attendance;
+import Enums.LessonType;
+import Enums.Semester;
+import Enums.Years;
+import Users.Student;
 
-public class Journal
-{
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Vector;
+
+public class Journal {
+
 	private Course course;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	private Date date;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	private Semester semester;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	private pair<Integer, Integer> year;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	private HashMap<Course, Vector<JournalLesson>> journalData;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public Journal(){
-		super();
+	private Years years;
+
+	private HashMap<Student, Vector<JournalLesson>> journalData;
+
+	public Journal() {
+
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void viewJournal(User parameter, JornalCourse parameter2) {
-		// TODO implement me
-		return null;	
+	public Journal(Course course, Semester semester, Years years) {
+		this.course = course;
+		this.semester = semester;
+		this.years = years;
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void updateJournalData(Teacher parameter, Course parameter2, Date parameter3, double parameter4, String parameter5) {
-		// TODO implement me
-		return null;	
+
+
+//	methods
+
+	public void addJournalLesson(Student student, JournalLesson lesson) {
+		try {
+			journalData.get(student).add(lesson);
+			System.out.println("Added Lesson " + lesson);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void updateJournalData(Teacher parameter, Course parameter2, Date parameter3, Attendance parameter4, String parameter5) {
-		// TODO implement me
-		return null;	
+
+
+//	update journal
+	public void updateJournalData(Student s, Date date, Attendance attendance) {
+		Vector<JournalLesson> lessons = journalData.get(s);
+		try {
+			for (JournalLesson lesson : lessons) {
+				if (lesson.getDate().equals(date)) {
+					lesson.setAttendance(attendance);
+					System.out.println("Updated Lesson " + lesson);
+					return;
+				}
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void updateJournalData(JournalCourse parameter, Attendance parameter2) {
-		// TODO implement me
-		return null;	
+
+	public void updateJournalData(Student s, Date date, double mark) {
+		Vector<JournalLesson> lessons = journalData.get(s);
+		try {
+			for (JournalLesson lesson : lessons) {
+				if (lesson.getDate().equals(date)) {
+					lesson.setMark(mark);
+					System.out.println("Updated Lesson " + lesson);
+					return;
+				}
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void updateJournalData(Teacher parameter, Course parameter2, Date parameter3, Attendance parameter4, double parameter5, String parameter6) {
-		// TODO implement me
-		return null;	
+
+	public void updateJournalData(Student s, Date date, String comment) {
+		Vector<JournalLesson> lessons = journalData.get(s);
+		try {
+			for (JournalLesson lesson : lessons) {
+				if (lesson.getDate().equals(date)) {
+					lesson.setComment(comment);
+					System.out.println("Updated Lesson " + lesson);
+					return;
+				}
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
-	
+
+	public void updateJournalData(Student s, Date date, LessonType type) {
+		Vector<JournalLesson> lessons = journalData.get(s);
+		try {
+			for (JournalLesson lesson : lessons) {
+				if (lesson.getDate().equals(date)) {
+					lesson.setLessonType(type);
+					System.out.println("Updated Lesson " + lesson);
+					return;
+				}
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	public void updateJournalData(Student s, Date date, Date newDate) {
+		Vector<JournalLesson> lessons = journalData.get(s);
+		try {
+			for (JournalLesson lesson : lessons) {
+				if (lesson.getDate().equals(date)) {
+					lesson.setDate(newDate);
+					System.out.println("Updated Lesson " + lesson);
+					return;
+				}
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+
+//	getters
+	public Course getCourse() {
+		return course;
+	}
+
+	public Semester getSemester() {
+		return semester;
+	}
+
+	public Years getYears() {
+		return years;
+	}
+
+	public HashMap<Student, Vector<JournalLesson>> getJournalData() {
+		return journalData;
+	}
+
+
+
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if(o == null || getClass() != o.getClass()) return false;
+		Journal j = (Journal) o;
+		return semester == j.semester
+				&& years == j.years
+				&& course == j.course;
+	}
+
+	@Override
+	public int hashCode() {
+		int res = 31;
+		res = res * 31 + (semester == null ? 0 : semester.hashCode());
+		res = res * 31 + (years == null ? 0 : years.hashCode());
+		res = res * 31 + (course == null ? 0 : course.hashCode());
+
+		return res;
+	}
+
+	@Override
+	public String toString() {
+		return "Journal " + years + " " + semester + " " + course;
+	}
 }
 
--- src/Academic/JournalLesson.java
+++ src/Academic/JournalLesson.java
@@ -1,67 +1,109 @@
 package Academic ;
 
+import Enums.Attendance;
+import Enums.LessonType;
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
+import java.util.Date;
 
-public class JournalLesson
-{
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+public class JournalLesson {
 	
 	private Date date;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+
 	
 	private LessonType lessonType;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+
 	
 	private Attendance attendance;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+
 	private double mark;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+
 	
 	private String comment;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
+
 	public JournalLesson(){
-		super();
+
 	}
 
+	public JournalLesson(Date date, LessonType lessonType) {
+		this.date = date;
+		this.lessonType = lessonType;
+	}
+
+	public JournalLesson(LessonType lessonType, Attendance attendance) {
+		this.date = new Date();
+		this.lessonType = lessonType;
+		this.attendance = attendance;
+	}
+
+	public JournalLesson(Date date, LessonType lessonType, Attendance attendance) {
+		this.date = date;
+		this.lessonType = lessonType;
+		this.attendance = attendance;
+	}
+
+	public JournalLesson(LessonType lessonType, Attendance attendance, double mark) {
+		this(lessonType, attendance);
+		this.mark = mark;
+	}
+
+	public JournalLesson(Date date, LessonType lessonType, Attendance attendance, double mark) {
+		this(date, lessonType, attendance);
+		this.mark = mark;
+	}
+
+
+	public JournalLesson(LessonType lessonType, Attendance attendance, double mark, String comment) {
+		this(lessonType, attendance, mark);
+		this.comment = comment;
+	}
+
+	public JournalLesson(Date date, LessonType lessonType, Attendance attendance, double mark, String comment) {
+		this(date, lessonType, attendance, mark);
+		this.comment = comment;
+	}
+
+
+//	getters and setters
+
+
+	public void setLessonType(LessonType lessonType) {
+		this.lessonType = lessonType;
+	}
+
+	public LessonType getLessonType() {
+		return lessonType;
+	}
+
+	public void setAttendance(Attendance attendance) {
+		this.attendance = attendance;
+	}
+
+	public Attendance getAttendance() {
+		return attendance;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setMark(double mark) {
+		this.mark = mark;
+	}
+
+	public double getMark() {
+		return mark;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Date getDate() {
+		return date;
+	}
 }
 
