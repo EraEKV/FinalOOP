86afb2b86835451b6dbb7f9a40657096525754d1
Abumuslim Abakarov <abakarovmuslim26@gmail.com>
2024-12-09 01:05:24 +0500

    upd

---
+++ src/Users/DisciplinaryCommittee.java
@@ -0,0 +1,34 @@
+package Users;
+
+import java.util.Vector;
+
+public class DisciplinaryCommittee extends Employee {
+
+    private int countOfMembers;
+
+    public DisciplinaryCommittee() {
+        super();
+        Vector<Complaint> complaints = new Vector<>();
+    }
+
+
+    public void makeWarning(Student student, Complaint complaint) {
+
+    }
+
+    public void kick(Student student) {
+
+    }
+
+    public void reviewCase(Complaint complaint) {
+
+    }
+
+    public void assignPenalty(Student student, Complaint complaint) {
+
+    }
+
+    public void deleteOrganization(Organization organization) {
+
+    }
+}
