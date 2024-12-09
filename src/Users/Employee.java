a60667a34bfb1ba8ce62f1903ee68585ebf7ae0d
Abumuslim Abakarov <abakarovmuslim26@gmail.com>
2024-12-09 22:10:30 +0500

    deleted message

--- src/Users/Employee.java
+++ src/Users/Employee.java
@@ -116,20 +116,3 @@ public class Employee extends User {
 	}
 }
 
-class Message {
-	private String senderId;
-	private String content;
-
-	public Message(String senderId, String content) {
-		this.senderId = senderId;
-		this.content = content;
-	}
-
-	public String getSenderId() {
-		return senderId;
-	}
-
-	public String getContent() {
-		return content;
-	}
-}
