39398e19945b3893c1d7b6f99645772397cca3e4
Nuridin <nurmannuridin28@gmail.com>
2024-12-13 22:10:18 +0500

    CustomPair v2

--- src/System/CustomPair.java
+++ src/System/CustomPair.java
@@ -1,4 +1,35 @@
 package System;
 
-public class CustomPair {
+public class CustomPair<F, S> {
+    private F first;
+    private S second;
+
+    public CustomPair() {
+    }
+
+    public CustomPair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public void setFirst(F first) {
+        this.first = first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public void setSecond(S second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "first = " + first + ", second = " + second;
+    }
 }
