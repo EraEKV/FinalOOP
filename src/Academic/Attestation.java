30d87a08c34eae421ac19d08e245d2b1d97ead61
Nuridin <nurmannuridin28@gmail.com>
2024-12-13 22:22:19 +0500

    update Attestation, but needed getNumericValue in AttMark

--- src/Academic/Attestation.java
+++ src/Academic/Attestation.java
@@ -2,16 +2,40 @@ package Academic;
 
 import java.util.*;
 import System.CustomPair;
+import Users.Student;
 
 public class Attestation {
+	private Student owner;
 	private Map<SemesterPeriod, List<CustomPair<Course, AttestationMark>>> info;
-
-	public Attestation() {
+	{
 		this.info = new HashMap<>();
+	}
+	public Attestation() {
+	}
+
+	public Attestation(Student owner) {
+		this.owner = owner;
+	}
+
+	public Attestation(Student owner, Map<SemesterPeriod, List<CustomPair<Course, AttestationMark>>> info) {
+		this.owner = owner;
+		this.info = info;
+	}
+
+	public Student getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Student owner) {
+		this.owner = owner;
 	}
 
 	public Map<SemesterPeriod, List<CustomPair<Course, AttestationMark>>> getInfo() {
 		return info;
+	}
+
+	public void setInfo(Map<SemesterPeriod, List<CustomPair<Course, AttestationMark>>> info) {
+		this.info = info;
 	}
 
 	public void updateAttestation(SemesterPeriod period, Course course, AttestationMark mark) {
@@ -27,6 +51,28 @@ public class Attestation {
 		}
 		attestationList.add(new CustomPair<>(course, mark));
 	}
+
+	public void calculateAverageMark() {
+		double totalMarks = 0;
+		int count = 0;
+
+		for (Map.Entry<SemesterPeriod, List<CustomPair<Course, AttestationMark>>> entry : info.entrySet()) {
+			for (CustomPair<Course, AttestationMark> pair : entry.getValue()) {
+				AttestationMark mark = pair.getSecond();
+				totalMarks += mark.getNumericValue();
+				count++;
+			}
+		}
+
+		if (count == 0) {
+			System.out.println("No marks available to calculate average.");
+			return;
+		}
+
+		double average = totalMarks / count;
+		System.out.println("Average Mark: " + average);
+	}
+
 
 	@Override
 	public String toString() {
