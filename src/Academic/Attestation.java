a1d128a1f1a39fe9dab6468d8c3a98cf9400a0f1
Nuridin <nurmannuridin28@gmail.com>
2024-12-16 22:46:08 +0500

    changes in Attestation

--- src/Academic/Attestation.java
+++ src/Academic/Attestation.java
@@ -5,73 +5,75 @@ import System.CustomPair;
 import Users.Student;
 
 public class Attestation {
-	private Student owner;
-	private Map<SemesterPeriod, List<CustomPair<Course, AttestationMark>>> info;
+	private Student student;
+	SemesterPeriod semesterPeriod;
+	private Map<Course, AttestationMark> info;
 	{
 		this.info = new HashMap<>();
 	}
 	public Attestation() {
 	}
 
-	public Attestation(Student owner) {
-		this.owner = owner;
+	public Attestation(Student student) {
+		this.student = student;
 	}
 
-	public Attestation(Student owner, Map<SemesterPeriod, List<CustomPair<Course, AttestationMark>>> info) {
-		this.owner = owner;
+	public Attestation(Student student, Map<Course, AttestationMark> info) {
+		this.student = student;
 		this.info = info;
 	}
 
-	public Student getOwner() {
-		return owner;
+	public Attestation(Student student, SemesterPeriod semesterPeriod, Map<Course, AttestationMark> info) {
+		this(student, info);
+		this.info = info;
 	}
 
-	public void setOwner(Student owner) {
-		this.owner = owner;
+	public Student getStudent() {
+		return student;
 	}
 
-	public Map<SemesterPeriod, List<CustomPair<Course, AttestationMark>>> getInfo() {
+	public Map<Course, AttestationMark> getInfo() {
 		return info;
 	}
 
-	public void setInfo(Map<SemesterPeriod, List<CustomPair<Course, AttestationMark>>> info) {
+	public void setInfo(Map<Course, AttestationMark> info) {
 		this.info = info;
 	}
 
-	public void updateAttestation(SemesterPeriod period, Course course, AttestationMark mark) {
-		info.putIfAbsent(period, new ArrayList<>());
+//	public void updateAttestation(SemesterPeriod period, Course course, AttestationMark mark) {
+//		info.putIfAbsent(period, new ArrayList<>());
+//
+//		List<CustomPair<Course, AttestationMark>> attestationList = info.get(period);
+//
+//		for (CustomPair<Course, AttestationMark> entry : attestationList) {
+//			if (entry.getFirst().equals(course)) {
+//				entry.setSecond(mark);
+//				return;
+//			}
+//		}
+//		attestationList.add(new CustomPair<>(course, mark));
+//	}
 
-		List<CustomPair<Course, AttestationMark>> attestationList = info.get(period);
-
-		for (CustomPair<Course, AttestationMark> entry : attestationList) {
-			if (entry.getFirst().equals(course)) {
-				entry.setSecond(mark);
-				return;
-			}
-		}
-		attestationList.add(new CustomPair<>(course, mark));
-	}
-
-	public void calculateAverageMark() {
-		double totalMarks = 0;
-		int count = 0;
-
-		for (Map.Entry<SemesterPeriod, List<CustomPair<Course, AttestationMark>>> entry : info.entrySet()) {
-			for (CustomPair<Course, AttestationMark> pair : entry.getValue()) {
-				AttestationMark mark = pair.getSecond();
-				totalMarks += mark.getNumericValue();
-				count++;
-			}
-		}
-
-		if (count == 0) {
-			System.out.println("No marks available to calculate average.");
-			return;
-		}
-
-		double average = totalMarks / count;
-		System.out.println("Average Mark for " + owner.getId() + " is  : " + average);
-	}
+//	public void calculateAverageMark() {
+//		double totalMarks = 0;
+//		int count = 0;
+//
+//		for (Map.Entry<SemesterPeriod, List<CustomPair<Course, AttestationMark>>> entry : info.entrySet()) {
+//			for (CustomPair<Course, AttestationMark> pair : entry.getValue()) {
+//				AttestationMark mark = pair.getSecond();
+//				totalMarks += mark.getNumericValue();
+//				count++;
+//			}
+//		}
+//
+//		if (count == 0) {
+//			System.out.println("No marks available to calculate average.");
+//			return;
+//		}
+//
+//		double average = totalMarks / count;
+//		System.out.println("Average Mark for " + student.getId() + " is  : " + average);
+//	}
 
 
 	@Override
