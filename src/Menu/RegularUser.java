0ef995499af6bee5b3a27cf0bee184c180dd6a7d
Abumuslim Abakarov <abakarovmuslim26@gmail.com>
2024-12-17 03:44:17 +0500

    deleted unused class

--- src/Menu/RegularUser.java
+++
@@ -1,8 +0,0 @@
-package Menu;
-import Users.User;
-
-public class RegularUser extends User {
-    public RegularUser(String firstname, String lastname, String email) {
-        super(firstname, lastname, email);
-    }
-}
